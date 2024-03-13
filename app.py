f546c8c19793b1d7d0b275445b95ea191e4e79d9
Marsel Christian <162213841+marsel-jr@users.noreply.github.com>
2024-03-13 17:51:13 +0700

    Update app.py

--- app.py
+++ app.py
@@ -18,13 +18,6 @@ data_hour, data_day = load_data()
 filtered_data_1 = data_day[(data_day["yr"] == 1) & (data_day["season"] == 3)]
 total_sewa_sepeda_1 = filtered_data_1["cnt"].sum()
 
-# Pertanyaan 2: Berapa banyak sepeda sewaan yang digunakan pada hari libur (liburan = 1) selama musim panas (musim 2) pada tahun 2011?
-filtered_data_2 = data_day[(data_day["yr"] == 0) & (data_day["season"] == 2) & (data_day["holiday"] == 1)]
-total_sepeda_sewaan_2 = filtered_data_2["cnt"].sum()
-
-# Pertanyaan 3: Bagaimana cara meningkatkan jumlah rental sepeda yang digunakan oleh pengguna biasa (casual) pada hari kerja (hari kerja = 1)?
-filtered_data_3 = data_day[(data_day["workingday"] == 1) & (data_day["casual"] > 0)]
-
 # Create Streamlit app
 st.title("Proyek Akhir Dicoding")
 st.header("Bike Sharing Dashboard")
@@ -49,11 +42,12 @@ selected_question = st.selectbox(
 # Tampilkan pertanyaan yang dipilih
 if selected_question == "Pertanyaan 1":
     st.header("Pertanyaan 1:")
-    st.write(f"Jumlah total sewa sepeda untuk tahun 2012 selama musim gugur (musim 3): {total_sewa_sepeda}")
+    st.write(f"Jumlah total sewa sepeda untuk tahun 2012 selama musim gugur (musim 3): {total_sewa_sepeda_1}")
     # Filter tahun 2012 dan musim gugur (season 3)
     filtered_data = data_day[(data_day["yr"] == 1) & (data_day["season"] == 3)]
     # Hitung jumlah total sewa sepeda (cnt)
     total_sewa_sepeda = filtered_data["cnt"].sum()
+    st.write(f"Jumlah total sewa sepeda untuk tahun 2012 selama musim gugur (musim 3): {total_sewa_sepeda}")
     # Create a bar chart (visualisasi)
     fig = px.bar(filtered_data, x=filtered_data["dteday"], y=filtered_data["cnt"], hover_data=["dteday", "cnt"])
     fig.update_xaxes(title="Tanggal")
@@ -62,11 +56,12 @@ if selected_question == "Pertanyaan 1":
     st.plotly_chart(fig)
 elif selected_question == "Pertanyaan 2":
     st.header("Pertanyaan 2:")
-    st.write(f"Jumlah total sepeda sewaan yang digunakan pada hari libur selama musim panas tahun 2011: {total_sepeda_sewaan}")
+    st.write(f"Jumlah total sepeda sewaan yang digunakan pada hari libur (liburan = 1) selama musim panas tahun 2011: {total_sepeda_sewaan_2}")
     # Filter tahun 2011, musim panas (season 2), dan hari libur (holiday = 1)
     filtered_data = data_day[(data_day["yr"] == 0) & (data_day["season"] == 2) & (data_day["holiday"] == 1)]
     # Hitung jumlah total sepeda sewaan
     total_sepeda_sewaan = filtered_data["cnt"].sum()
+    st.write(f"Jumlah total sepeda sewaan yang digunakan pada hari libur selama musim panas tahun 2011: {total_sepeda_sewaan}")
     fig = px.bar(filtered_data, x=filtered_data["dteday"], y=filtered_data["cnt"], hover_data=["dteday", "cnt"])
     fig.update_xaxes(title="Tanggal")
     fig.update_yaxes(title="Jumlah Sepeda Sewaan")
